982fbaccf4ff35979d9dc81f692fb513e7a737b3
Radik Zainullin <radik.zainullin@gmail.com>
2023-11-12 19:51:30 +0300

    подсчет факториала через цикл

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,9 +1,21 @@
-// Вычислить факториал
-function factorial(n) {
+// Вычислить факториал вариант 1
+/* function factorial(n) {
   if (n === 1 || n === 0) {
     return 1;
   }
   if (n > 1) {
     return n * factorial(n - 1);
   }
+} */
+
+// Вычислить факториал вариант 2
+function factorial(n) {
+  if (n === 1 || n === 0) {
+    return 1;
+  }
+  let result = 1;
+  for (let i = 2; i < n + 1; i++) {
+    result *= i;
+  }
+  return result;
 }
