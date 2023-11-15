430304324d034bd1b6530d09c01070c9ea271735
Radik Zainullin <radik.zainullin@gmail.com>
2023-11-15 22:04:23 +0300

    check spam

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,3 +1,13 @@
+// Напишите функцию `checkSpam(str)`, возвращающую `true`, если `str` содержит `'1xBet'` или `'XXX'`, а иначе `false`.
+
+// Функция должна быть нечувствительна к регистру:
+
+// ```js
+// checkSpam('1XbeT now') === true
+// checkSpam('free xxxxx') === true
+// checkSpam('innocent rabbit') === false
+
 function checkSpam(str) {
-  // ваш код...
+  const regexp = /1xBet|XXX/i;
+  return regexp.test(str);
 }
