3d5aefe3c946e0384acece03553f140d6bfcad49
Radik Zainullin <radik.zainullin@gmail.com>
2023-11-19 18:26:43 +0300

    сделаны три задачи из модуля 2

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,10 @@
 function sumSalary(salaries) {
-  // ваш код...
+  let result = 0;
+  for (const key in salaries) {
+    const val = Number(salaries[key]);
+    if (!Number.isNaN(val) && isFinite(val)) {
+      result += Number(salaries[key]);
+    }
+  }
+  return result;
 }
