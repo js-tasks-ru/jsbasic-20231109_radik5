78299480ab8539fae19091cfbfe900208b5c7a37
Radik Zainullin <radik.zainullin@gmail.com>
2023-11-20 16:46:54 +0300

    исправление 1-й задачи из 3-го модуля

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,9 +1,9 @@
 function sumSalary(salaries) {
   let result = 0;
   for (const key in salaries) {
-    const val = Number(salaries[key]);
-    if (!Number.isNaN(val) && isFinite(val)) {
-      result += Number(salaries[key]);
+    const value = salaries[key];
+    if (typeof value === "number" && isFinite(value)) {
+      result += value;
     }
   }
   return result;
